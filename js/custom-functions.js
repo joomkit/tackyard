a5e274c2c1169f65747b5dbac513f0c9acc40999
joomkit <info@joomkit.com>
2022-09-14 21:54:55 +0100

    Update custom-functions.js

--- js/custom-functions.js
+++ js/custom-functions.js
@@ -13,13 +13,15 @@ document.addEventListener('DOMContentLoaded', function (event) {
             if (checked) {
                 // shipping custom help
                 var fieldtitle = 'Shipping';
-                var infotext = 'See our useful shipping your stuff guide <a class="jsModalTrigger" href="jsModal">here</a>'
+                var infotext = 'See our useful shipping your stuff guide <a target="_blank" class="jsModalTrigger" href="jsModal">here</a>'
                 var labels = document.getElementsByTagName('LABEL');
                 for (var i = 0; i < labels.length; i++) {
                     if (labels[i].textContent == fieldtitle) {
                         labels[i].insertAdjacentHTML('afterend', '<div id="ship-help" class="info-text-container"><div class="info-text-icon"><i class="ss-info"></i></div><div class="info-text-content"><p>' + infotext + '</p></div></div>');
                     }
                 }
+                ready(openModal);
+                ready(closeModal);
             } else {
                 let node = document.getElementById("ship-help");
                 if (node.parentNode) {
@@ -67,7 +69,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
     console.log(found);
 
     if (found !== null && found[0] === find) {
-        console.log("update location text info gh");
+        console.log("update location text info");
         let fieldtitle = 'Location';
         let infotext = 'Add your postcode or town to show buyers where you are.';
         let alllabels = document.getElementsByTagName('LABEL');
@@ -80,3 +82,60 @@ document.addEventListener('DOMContentLoaded', function (event) {
     }
 });
 
+// modal
+/* This script supports IE9+ */
+(function() {
+    /* Opening modal window function */
+    function openModal() {
+        /* Get trigger element */
+        var modalTrigger = document.getElementsByClassName('jsModalTrigger');
+  
+        /* Set onclick event handler for all trigger elements */
+        for(var i = 0; i < modalTrigger.length; i++) {
+            modalTrigger[i].onclick = function() {
+              var target = this.getAttribute('href').substr(1);
+              var modalWindow = document.getElementById(target);
+  
+              modalWindow.classList ? modalWindow.classList.add('open') : modalWindow.className += ' ' + 'open'; 
+            }
+        }   
+    }
+  
+    function closeModal(){
+      /* Get close button */
+      var closeButton = document.getElementsByClassName('jsModalClose');
+      var closeOverlay = document.getElementsByClassName('jsOverlay');
+  
+      /* Set onclick event handler for close buttons */
+        for(var i = 0; i < closeButton.length; i++) {
+          closeButton[i].onclick = function() {
+            var modalWindow = this.parentNode.parentNode;
+  
+            modalWindow.classList ? modalWindow.classList.remove('open') : modalWindow.className = modalWindow.className.replace(new RegExp('(^|\\b)' + 'open'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+          }
+        }   
+  
+      /* Set onclick event handler for modal overlay */
+        for(var i = 0; i < closeOverlay.length; i++) {
+          closeOverlay[i].onclick = function() {
+            var modalWindow = this.parentNode;
+  
+            modalWindow.classList ? modalWindow.classList.remove('open') : modalWindow.className = modalWindow.className.replace(new RegExp('(^|\\b)' + 'open'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+          }
+        }  
+  
+    }
+  
+    /* Handling domready event IE9+ */
+    function ready(fn) {
+      if (document.readyState != 'loading'){
+        fn();
+      } else {
+        document.addEventListener('DOMContentLoaded', fn);
+      }
+    }
+  
+    /* Triggering modal window function after dom ready */
+    ready(openModal);
+    ready(closeModal);
+  }());
