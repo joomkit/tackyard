72d8201b5e298484f68d6d4669f7164090b137dc
joomkit <info@joomkit.com>
2022-09-15 16:17:34 +0100

    listen

--- js/custom-functions.js
+++ js/custom-functions.js
@@ -6,12 +6,13 @@ var closeButton;
 
 // }
 
-function initModal(){
-   modal = document.querySelector(".modal");
-   trigger = document.querySelector(".trigger");
-   closeButton = document.querySelector(".close-button");
+function initModal() {
+  modal = document.querySelector(".modal");
+  trigger = document.querySelector(".trigger");
+  closeButton = document.querySelector(".close-button");
 }
-function initListeners(){
+function initListeners() {
+  trigger.addEventListener("click", toggleModal);
   closeButton.addEventListener("click", toggleModal);
   window.addEventListener("click", windowOnClick);
 
@@ -40,9 +41,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
       const checked = document.querySelector('#shipping-checkbox:checked') !== null;
       console.log(checked); // false
       if (checked) {
-        // init modal button
-        initModal()
-        initListeners()
+
         // shipping custom help
         var fieldtitle = 'Shipping';
         var infotext = 'See our useful shipping your stuff guide <button id="ship-pop"  class="trigger" >here</button>'
@@ -52,6 +51,9 @@ document.addEventListener('DOMContentLoaded', function (event) {
             labels[i].insertAdjacentHTML('afterend', '<div id="ship-help" class="info-text-container"><div class="info-text-icon"><i class="ss-info"></i></div><div class="info-text-content"><p>' + infotext + '</p></div></div>');
           }
         }
+        // init modal button
+        initModal()
+        initListeners()
       } else {
         let node = document.getElementById("ship-help");
         if (node.parentNode) {
@@ -107,12 +109,12 @@ document.addEventListener('DOMContentLoaded', function (event) {
     let infotextLoc = 'Add your postcode or town to show buyers where you are.';
 
     // if (labels) {
-      for (var i = 0; i < labels.length; i++) {
-        console.log('label-loop');
-        if (labels[i].textContent == LocFieldtitle) {
-          labels[i].insertAdjacentHTML('afterend', '<div class="info-text-container"><div class="info-text-icon"><i class="ss-info"></i></div><div class="info-text-content"><p>' + infotextLoc + '</p></div></div>');
-        }
+    for (var i = 0; i < labels.length; i++) {
+      console.log('label-loop');
+      if (labels[i].textContent == LocFieldtitle) {
+        labels[i].insertAdjacentHTML('afterend', '<div class="info-text-container"><div class="info-text-icon"><i class="ss-info"></i></div><div class="info-text-content"><p>' + infotextLoc + '</p></div></div>');
       }
+    }
     // }
   }
 });
